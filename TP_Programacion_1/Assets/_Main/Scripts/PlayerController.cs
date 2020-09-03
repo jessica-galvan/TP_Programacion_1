ef4b6e692b6385be8a19066f7ffe35ef0e819dc6
Jessica <jessica.galvan@hotmail.com>
2020-09-02 22:39:18 -0300

    Assets + Cinemachine + Foldes + others
    Y creado algunas carpetas, y empezado algunos scripts.

---
+++ TP_Programacion_1/Assets/_Main/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("Health Settings")]
+    [SerializeField] private int maxLife;
+    private int currentLife;
+    public LifeController lifeController;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxLife = currentLife;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MOVEMENT
+        if (Input.GetKey("left"))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("right"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+    }
+}
